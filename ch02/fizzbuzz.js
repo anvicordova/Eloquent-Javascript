0886a7c158715d964a0817cfe4dbb886a4deb3fc
Angela Córdova G <anvicordova@gmail.com>
2018-08-08 23:25:06 -0500

    Fizzbuzz exercise

---
+++ ch02/fizzbuzz.js
@@ -0,0 +1,6 @@
+for(let number = 1; number < 101; number += 1){
+  let output = ""
+  if(number % 3 == 0) output += "Fizz"
+  if(number % 5 == 0) output += "Buzz"
+  console.log(output || number)
+}
