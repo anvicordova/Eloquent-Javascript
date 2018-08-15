7dde15a171421f1f624400f09bfddb2ba4c70683
Angela Córdova G <anvicordova@gmail.com>
2018-08-14 22:55:26 -0500

    recursion.js

---
+++ ch03/recursion.js
@@ -0,0 +1,10 @@
+function isEven(n){
+  if(n == 0)  return true;
+  else if(n == 1)  return false;
+  else return isEven(Math.abs(n - 2));
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-100));
